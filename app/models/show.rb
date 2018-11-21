cc8ab347a5cc58d3ea9b90ffeb7b809f9137d6e2
Austin  Burke <aburk3@gmail.com>
2018-11-21 19:30:35 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
-  def self.highest_rating
-    self.maximum(:rating)
-  end
+  # def self.highest_rating
+  #   self.maximum(:rating)
+  # end
 
   # def self.most_popular_show
   #   self.where("rating = ?", self.highest_rating).first
