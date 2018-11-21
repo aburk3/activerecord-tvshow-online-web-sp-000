1411c4659a744e231df516562230c3bddba248ae
Austin  Burke <aburk3@gmail.com>
2018-11-21 19:44:43 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 class Show < ActiveRecord::Base
   def self.highest_rating
-    binding.pry
-    # self.maximum(:rating)
+    self.maximum(:rating)
   end
-  #
-  # def self.most_popular_show
-  #   self.where("rating = ?", self.highest_rating).first
-  # end
+
+  def self.most_popular_show
+    binding.pry
+    self.where("rating = ?", self.highest_rating).first
+  end
   #
   # def self.lowest_rating
   #   self.minimum(:rating)
