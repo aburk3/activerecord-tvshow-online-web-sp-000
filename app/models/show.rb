189045d51730173677287169cf3e2b58cdcd3545
Austin  Burke <aburk3@gmail.com>
2018-11-21 19:51:38 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -22,9 +22,9 @@ class Show < ActiveRecord::Base
     self.sum(:rating)
   end
 
-  # def self.popular_shows
-  #   self.where("rating >= ?", 5)
-  # end
+  def self.popular_shows
+    self.where("rating >= ?", 5)
+  end
   #
   # def self.shows_by_alphabetical_order
   #   self.order(:name)
